1b7cc3cacabf1f2bd2e410bc1e6fe8b5a603a7fe
DAVID DEBOER <ddeboer@berkeley.edu>
2024-12-31 10:21:01 -0800

    change calfile input to tk

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ATA Observation Calendar
+
+ATA Observing calendar - calendar info is in json files delineated by year, e.g. cal2024.json
--- scripts/aoctkuser.py
+++ scripts/aoctkuser.py
@@ -4,7 +4,7 @@ import argparse
 from obscalendar.tk_obscalendar import ObservingCalendarApp
 
 ap = argparse.ArgumentParser()
-ap.add_argument('--calfile', help="Calendar file to use/find.", default='now')
+ap.add_argument('calfile', help="Calendar file to use/find.", nargs='?', default='now')
 ap.add_argument('--path', help="path to use", default='getenv')
 ap.add_argument('--output', help="Output logging level", default='INFO')
 args = ap.parse_args()
