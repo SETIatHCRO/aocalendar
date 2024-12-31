1d6cec82c82454c563b2ec64cbb4138852254104
DAVID DEBOER <ddeboer@berkeley.edu>
2024-12-31 15:26:30 -0800

    update aocuser

--- scripts/aocuser.py
+++ scripts/aocuser.py
@@ -3,13 +3,16 @@ import argparse
 from obscalendar import obscalendar
 
 ap = argparse.ArgumentParser()
-ap.add_argument('calfile', help="Calfile to use.", nargs='?', default='now')
+ap.add_argument('calfile', help="Calfile/date to use.", nargs='?', default='now')
 ap.add_argument('--path', help="Path to use", default='getenv')
 ap.add_argument('--output', help="Logging output level", default='INFO')
 # Actions
-ap.add_argument('--show', help="Show contents of day", default=False)
-ap.add_argument('--entry', help="Show an entry YYYY-MM-DD:#", default=False)
-ap.add_argument('--graph', help="Graph calendar day", default=False)
+ap.add_argument('--show', help="Show contents of day", action='store_true')
+ap.add_argument('--entry', help="Show an entry # on date", default=False)
+ap.add_argument('--graph', help="Graph calendar day", action='store_true')
+ap.add_argument('--add', help="Add an entry", action='store_true')
+ap.add_argument('--update', help="Update an entry # on date", default=False)
+ap.add_argument('--delete', help="Delete an entry # on date", default=False)
 # Event fields
 ap.add_argument('--name', help="Event field", default='name')
 ap.add_argument('--id', help="Event field", default='ID')
@@ -22,20 +25,24 @@ ap.add_argument('--state', help="Event field", default='primary')
 
 args = ap.parse_args()
 
-if args.show:
-    args.calfile=args.show
-if args.entry:
-    args.calfile = args.entry.split(':')[0]
-if args.graph:
-    args.calfile = args.graph
+if args.add:
+    args.calfile = args.utc_start
 
 aoc = obscalendar.Calendar(calfile=args.calfile, path=args.path, output=args.output)
 
 if args.show:
-    print(aoc.format_day_contents(day=args.show, cols='all', return_as='table'))
+    print(aoc.format_day_contents(day=args.calfile, cols='all', return_as='table'))
 if args.entry:
-    this_entry_key, num = obscalendar.split_entry(args.entry)
-    print(aoc.contents[this_entry_key][num])
+    print(aoc.contents[args.calfile][int(args.entry)])
 if args.graph:
-    print(aoc.graph_day(day=args.graph))
+    print(aoc.graph_day(day=args.calfile))
     print("\n\n")
+if args.add:
+    aoc.edit('add', **vars(args))
+    aoc.write_calendar()
+if args.update:
+    aoc.edit('update', entry=f"{args.calfile}:{args.update}", **vars(args))
+    aoc.write_calendar()
+if args.delete:
+    aoc.edit('delete', entry=f"{args.calfile}:{args.delete}")
+    aoc.write_calendar()
