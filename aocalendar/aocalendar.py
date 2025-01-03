e904997e95e3d902e54bdd128c518c402cde995d
DAVID DEBOER <ddeboer@berkeley.edu>
2025-01-03 11:19:08 -0800

    fixed up aocuser schedule

--- aocalendar/aocalendar.py
+++ aocalendar/aocalendar.py
@@ -633,12 +633,16 @@ class Calendar:
         kwargs['utc_stop'] = srcstop.datetime.isoformat(timespec='seconds')
         rastr, decstr = f"{ra.hms.h:.0f}h{ra.hms.m:.0f}m{ra.hms.s:.0f}s", f"{dec.dms.d:.0f}d{dec.dms.m:.0f}m{dec.dms.s:.0f}s"
         radec = f"{rastr},{decstr}"
+        print("BEFORE")
+        print(kwargs)
         if 'name' not in kwargs:  kwargs['name'] = radec
         kwargs.setdefault('note', '')
         if not isinstance(kwargs['note'], str): kwargs['note'] = radec
         else: kwargs['note'] += f" -- {radec}"
         self.add(**kwargs)
         logger.warning("Now should edit down the scheduled observation times!")
+        print("AFTER")
+        print(kwargs)
         return True
 
     def conflicts(self, check_event, is_new=False):
